9c66d29e5e5feb4bf990be2df0361123c2bdd894
seynabousall <seynabousall01@gmail.com>
2023-01-26 12:34:48 +0100

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var menuHmburger = document.querySelector('.collapse');
+var voirPlus = document.querySelector('.plus2');
+var voirPlusButton = document.querySelector('.voir');
+
+function show () { 
+    menuHmburger.classList.toggle('collapse1') 
+}
+function showVoirPLus () { 
+    voirPlus.classList.toggle('plus3') 
+     
+}
+
